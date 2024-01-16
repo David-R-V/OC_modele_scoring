41c5f6fd9a570959285069671d64daa10696e482
David Rofes Vernis <d.rofesvernis@gmail.com>
2024-01-16 19:12:53 +0100

    script api yc partie joblib

--- 2_backend/API.py
+++ 2_backend/API.py
@@ -82,83 +82,83 @@ async def get_html_content():
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="HTML file not found")
 
-# Endpoint qui renvoie la proba associée à l'ID sélectionnée
-# @app.get("/model_predictif/{index_cible}")
+Endpoint qui renvoie la proba associée à l'ID sélectionnée
+@app.get("/model_predictif/{index_cible}")
 
-# async def model_predictif(index_cible: int) -> List[float] :
+async def model_predictif(index_cible: int) -> List[float] :
          
-#     file_path = "2_backend/API_input_data/base_futur_prep_algo.csv"
-#     separator = '\t'
-#     compression = 'gzip'
+    file_path = "2_backend/API_input_data/base_futur_prep_algo.csv"
+    separator = '\t'
+    compression = 'gzip'
 
-#     try:
-#         line_number = serie_index_clients.index.get_loc(index_cible) + 1
-#     except KeyError:
-#         raise HTTPException(status_code=404, detail=f"Index {index_cible} not found")
+    try:
+        line_number = serie_index_clients.index.get_loc(index_cible) + 1
+    except KeyError:
+        raise HTTPException(status_code=404, detail=f"Index {index_cible} not found")
 
-#     with gzip.open(file_path, mode='rt', encoding='latin1') as file:
-#         selected_line = next(islice(file, line_number, line_number + 1), None)
+    with gzip.open(file_path, mode='rt', encoding='latin1') as file:
+        selected_line = next(islice(file, line_number, line_number + 1), None)
 
-#     if selected_line is not None:
-#         if selected_line.endswith('\n') and not selected_line.strip().endswith(','):
-#             selected_line = selected_line.rstrip('\n')
+    if selected_line is not None:
+        if selected_line.endswith('\n') and not selected_line.strip().endswith(','):
+            selected_line = selected_line.rstrip('\n')
 
-#         selected_line = pd.DataFrame(selected_line.split(separator),
-#                                      index=pd.read_csv(file_path, sep=separator, compression=compression, nrows=1).columns.to_list()).T.set_index('ID')
-#         selected_line = selected_line.replace('', np.nan)
-#         selected_line = selected_line.drop(['TARGET','INDEX','SK_ID_CURR'],axis=1)
-#         selected_line = selected_line.round(3)
+        selected_line = pd.DataFrame(selected_line.split(separator),
+                                     index=pd.read_csv(file_path, sep=separator, compression=compression, nrows=1).columns.to_list()).T.set_index('ID')
+        selected_line = selected_line.replace('', np.nan)
+        selected_line = selected_line.drop(['TARGET','INDEX','SK_ID_CURR'],axis=1)
+        selected_line = selected_line.round(3)
 
-#         # on a techniquement besoin que de la proba de classe 1 : si >0.5, classe 1 = pb de paiement; si <0.5, classe 0
-#         proba_risk = model_risk.predict_proba(selected_line.values.reshape(1, -1))[:,1]
+        # on a techniquement besoin que de la proba de classe 1 : si >0.5, classe 1 = pb de paiement; si <0.5, classe 0
+        proba_risk = model_risk.predict_proba(selected_line.values.reshape(1, -1))[:,1]
 
-#         return proba_risk
+        return proba_risk
     
-#     else:
-#         return Response(content={"message": "Erreur: index inconnu"})
+    else:
+        return Response(content={"message": "Erreur: index inconnu"})
 
-# #Import de l'explication SHAP
-# try:
-#     explanation = joblib.load("2_backend/API_input_data/shap_explanation_model_risk_100.joblib")
-# except Exception as e:
-#     raise HTTPException(status_code=500, detail=f"Error loading explanation file: {str(e)}")
+#Import de l'explication SHAP
+try:
+    explanation = joblib.load("2_backend/API_input_data/shap_explanation_model_risk_100.joblib")
+except Exception as e:
+    raise HTTPException(status_code=500, detail=f"Error loading explanation file: {str(e)}")
 
-# #Endpoint qui génère l'image Waterfall plot et renvoie le texte du chemin où elle se trouve
-# @app.get("/explain/{index_cible}")
-# async def explain(index_cible: int):
-#     try:
-#         line_number = serie_index_clients.index.get_loc(index_cible) + 1
-#     except KeyError:
-#         raise HTTPException(status_code=404, detail=f"Index {index_cible} not found")
+#Endpoint qui génère l'image Waterfall plot et renvoie le texte du chemin où elle se trouve
+@app.get("/explain/{index_cible}")
+async def explain(index_cible: int):
+    try:
+        line_number = serie_index_clients.index.get_loc(index_cible) + 1
+    except KeyError:
+        raise HTTPException(status_code=404, detail=f"Index {index_cible} not found")
 
-#     try:
-#         shap_values = explanation[line_number]
+    try:
+        shap_values = explanation[line_number]
         
-#         shap.waterfall_plot(shap_values,max_display=20,show=False)
-#         title=plt.title('',loc='left')
-#         title.set_position((-0.4,0))
-#         #title_text_obj = plt.text(-0.13,0.88, f"ID = {index_cible}", color='blue', fontsize=12, fontweight='bold', transform=fig.transFigure)
-#         plt.savefig('2_backend/API_input_data/shap_waterfall_plot.jpeg',bbox_inches='tight')
-#         plt.close()
+        shap.waterfall_plot(shap_values,max_display=20,show=False)
+        title=plt.title('',loc='left')
+        title.set_position((-0.4,0))
+        #title_text_obj = plt.text(-0.13,0.88, f"ID = {index_cible}", color='blue', fontsize=12, fontweight='bold', transform=fig.transFigure)
+        plt.savefig('2_backend/API_input_data/shap_waterfall_plot.jpeg',bbox_inches='tight')
+        plt.close()
         
-#         return "2_backend/API_input_data/shap_waterfall_plot.jpeg"
+        return "2_backend/API_input_data/shap_waterfall_plot.jpeg"
     
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))   
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))   
 
-#Endpoint qui génère l'image des features rangées par importance lgbm et renvoie le texte du chemin où elle se trouve
-# @app.get("/explain")
-# async def explain():
+Endpoint qui génère l'image des features rangées par importance lgbm et renvoie le texte du chemin où elle se trouve
+@app.get("/explain")
+async def explain():
 
-#     try: 
-#         lightgbm.plot_importance(model_risk,max_num_features=30,importance_type='split',figsize=(8,8),title='Classement des variables impactantes globales du modèle')
-#         plt.savefig('2_backend/API_input_data/lgbm_features_plot.jpeg',bbox_inches='tight')
-#         plt.close()
+    try: 
+        lightgbm.plot_importance(model_risk,max_num_features=30,importance_type='split',figsize=(8,8),title='Classement des variables impactantes globales du modèle')
+        plt.savefig('2_backend/API_input_data/lgbm_features_plot.jpeg',bbox_inches='tight')
+        plt.close()
         
-#         return "2_backend/API_input_data/lgbm_features_plot.jpeg"
+        return "2_backend/API_input_data/lgbm_features_plot.jpeg"
     
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e)) 
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) 
 
 
     
